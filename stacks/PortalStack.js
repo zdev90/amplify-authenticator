c3ad3614d830d3f34f61794503ef0bbb39dea37e
zdev90 <zdev209@hotmail.com>
2023-03-15 13:01:05 -0600

    add deploy configuration
    add lock files
    
    Add deploy configuration

--- stacks/PortalStack.js
+++ stacks/PortalStack.js
@@ -14,6 +14,13 @@ export function PortalStack({ stack, app }) {
   const { apiUrl, api } = use(ApiStack);
   const { configBucket, configObjectKey } = use(ConfigStack);
 
+  const stackOutputs = {
+    ConfigBucket: configBucket.bucketName
+  };
+
+  const returnValue = {};
+
+  console.log(app.stage);
   // Frontend auth app
   const portal = new StaticSite(stack, "AuthPortal", {
     path: "frontend/auth-app",
@@ -27,6 +34,10 @@ export function PortalStack({ stack, app }) {
       REACT_APP_USER_POOL_CLIENT_ID: auth.userPoolClientId,
     },
   });
+
+  stackOutputs["AuthAppUrl"] = portal.url || TEST_PORTAL_URL;
+  returnValue["portal"] = portal;
+  returnValue[""]
 
   const portalUrl = portal.url || TEST_PORTAL_URL;
 
@@ -43,45 +54,50 @@ export function PortalStack({ stack, app }) {
     }
   );
 
-  // Client frontend app1
-  const clientPortal1 = new StaticSite(stack, "ClientPortal1", {
-    path: "frontend/client-example1",
-    buildCommand: "npm run build",
-    buildOutput: "build",
-    environment: {
-      REACT_APP_REGION: app.region,
-      REACT_APP_USER_POOL_ID: auth.userPoolId,
-      REACT_APP_IDENTITY_POOL_ID: auth.cognitoIdentityPoolId || "",
-      REACT_APP_BROKER_URL: apiUrl,
-    },
-  });
+  returnValue["uploadConfig"] = uploadConfig;
 
-  // Client frontend app2
-  const clientPortal2 = new StaticSite(stack, "ClientPortal2", {
-    path: "frontend/client-example2",
-    buildCommand: "npm run build",
-    buildOutput: "build",
-    environment: {
-      REACT_APP_REGION: app.region,
-      REACT_APP_USER_POOL_ID: auth.userPoolId,
-      REACT_APP_IDENTITY_POOL_ID: auth.cognitoIdentityPoolId || "",
-      REACT_APP_BROKER_URL: apiUrl,
-    },
-  });
+  if (app.stage !== "production") {
+    // Client frontend app1
+    const clientPortal1 = new StaticSite(stack, "ClientPortal1", {
+      path: "frontend/client-example1",
+      buildCommand: "npm run build",
+      buildOutput: "build",
+      environment: {
+        REACT_APP_REGION: app.region,
+        REACT_APP_USER_POOL_ID: auth.userPoolId,
+        REACT_APP_IDENTITY_POOL_ID: auth.cognitoIdentityPoolId || "",
+        REACT_APP_BROKER_URL: apiUrl,
+      },
+    });
+
+    stackOutputs["ClientApp1Url"] = clientPortal1.url || TEST_CLIENT_PORTAL1_URL;
+    returnValue["clientPortal1"] = clientPortal1;
+
+    // Client frontend app2
+    const clientPortal2 = new StaticSite(stack, "ClientPortal2", {
+      path: "frontend/client-example2",
+      buildCommand: "npm run build",
+      buildOutput: "build",
+      environment: {
+        REACT_APP_REGION: app.region,
+        REACT_APP_USER_POOL_ID: auth.userPoolId,
+        REACT_APP_IDENTITY_POOL_ID: auth.cognitoIdentityPoolId || "",
+        REACT_APP_BROKER_URL: apiUrl,
+      },
+    });
+
+    stackOutputs["ClientApp2Url"] = clientPortal2.url || TEST_CLIENT_PORTAL2_URL;
+    returnValue["clientPortal2"] = clientPortal2;
+  }
+  
 
   // Show the endpoint in the output
-  stack.addOutputs({
-    AuthAppUrl: portalUrl,
-    ClientApp1Url: clientPortal1.url || TEST_CLIENT_PORTAL1_URL,
-    ClientApp2Url: clientPortal2.url || TEST_CLIENT_PORTAL2_URL,
-    ConfigBucket: configBucket.bucketName,
-  });
+  stack.addOutputs(stackOutputs);
 
-  return {
-    portal,
-    portalUrl,
-    clientPortal1,
-    clientPortal2,
-    uploadConfig,
-  };
+  return returnValue;
+}
+
+const portalValues = {
+  stackOutputs: {}
+
 }
